dd5320749c50e4b533d2185465fe63c7ffd3147c
klevisonRibeiro <klevisonribeiro@live.com>
2021-05-30 18:15:36 -0300

    instructions to create this project

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+==============================
 Basic CRUD API with node.js 
+==============================
+
+- instructions to create this project
+    > npm init -y
+    > npm install express mysql dotenv cors body-parser
+    > npm install nodemon --save-dev
+    > create database: file in the main folder (dataBaseSqlFile.sql)
+
---
+++ dataBaseSqlFile.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.25, for Win64 (x86_64)
+--
+-- Host: localhost    Database: clubsdatabase
+-- ------------------------------------------------------
+-- Server version	8.0.25
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `clubs`
+--
+
+DROP TABLE IF EXISTS `clubs`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clubs` (
+  `clubId` int NOT NULL AUTO_INCREMENT,
+  `clubName` varchar(100) NOT NULL,
+  `creationYear` int NOT NULL,
+  `clubCountry` varchar(50) NOT NULL,
+  `internationalTrophies` int DEFAULT NULL,
+  `continentalTrophies` int DEFAULT NULL,
+  `nationalTrophies` int DEFAULT NULL,
+  `regionalTrophies` int DEFAULT NULL,
+  PRIMARY KEY (`clubId`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clubs`
+--
+
+LOCK TABLES `clubs` WRITE;
+/*!40000 ALTER TABLE `clubs` DISABLE KEYS */;
+INSERT INTO `clubs` VALUES (1,'Cruzeiro',1921,'Brazil',NULL,7,10,59),(2,'AC Milan',1899,'Italy',4,14,32,NULL),(3,'Barcelona',1899,'Spain',3,19,75,36),(7,'Real Madrid',1902,'Spain',7,21,68,NULL),(10,'Chelsea',1905,'England',NULL,7,61,NULL),(16,'Atletico Mineiro',1908,'Brazil',NULL,4,5,64),(18,'PSG',1970,'France',NULL,2,43,NULL);
+/*!40000 ALTER TABLE `clubs` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-05-30 18:10:09
