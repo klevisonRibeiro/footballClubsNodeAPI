8635a149eea10c6725f310a511e747752d7f3b1a
klevisonRibeiro <klevisonribeiro@live.com>
2021-06-02 14:24:25 -0300

    ;

--- src/server.js
+++ src/server.js
@@ -6,8 +6,8 @@ const routes = require('./routes');
 const server = express();
 
 server.use(cors());
-server.use(express.json())
-server.use(express.urlencoded({ extended: true}))
+server.use(express.json());
+server.use(express.urlencoded({ extended: true}));
 server.use('/api', routes);
 
 server.listen(process.env.PORT, ()=> {
